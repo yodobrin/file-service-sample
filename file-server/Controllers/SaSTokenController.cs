a6987ea9741156920f80c0c981043c441de9dc16
Yoav Dobrin <yodobrin@microsoft.com>
2022-08-30 14:13:51 +0300

    akv + mi

--- file-server/Controllers/SaSTokenController.cs
+++ file-server/Controllers/SaSTokenController.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
-// using Azure.Identity;
-// using Azure.Security.KeyVault.Secrets;
-// using Azure.Core;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Azure.Core;
 
 namespace file_server.Controllers;
 
@@ -10,34 +10,43 @@ namespace file_server.Controllers;
 public class SaSTokenController : ControllerBase
 {
     private readonly ILogger<SaSTokenController> _logger;
-    // private readonly SecretClientOptions options = new SecretClientOptions()
-    //         {
-    //             Retry =
-    //             {
-    //                 Delay= TimeSpan.FromSeconds(2),
-    //                 MaxDelay = TimeSpan.FromSeconds(16),
-    //                 MaxRetries = 5,
-    //                 Mode = RetryMode.Exponential
-    //             }
-    //         };
-    // private SecretClient secretClient;
+    private readonly SecretClientOptions options = new SecretClientOptions()
+            {
+                Retry =
+                {
+                    Delay= TimeSpan.FromSeconds(2),
+                    MaxDelay = TimeSpan.FromSeconds(16),
+                    MaxRetries = 5,
+                    Mode = RetryMode.Exponential
+                }
+            };
+    private SecretClient secretClient;
     public SaSTokenController(ILogger<SaSTokenController> logger)
     {
         _logger = logger;
         // change this to be from config
-        // secretClient = new SecretClient(new Uri("https://fileuploadkv.vault.azure.net/"), new DefaultAzureCredential(),options);
+        var miCredentials = new DefaultAzureCredential(new DefaultAzureCredentialOptions
+                        {
+                            ManagedIdentityClientId = "7e07a243-0b11-490b-9ff5-bd188b369cc7"
+                        });
+        secretClient = new SecretClient(new Uri("https://fileuploadkv.vault.azure.net/"), 
+                                        new DefaultAzureCredential(),
+                                        options);
     }
 
     [HttpGet(Name = "GetSaSToken")]
     public string GetSaSToken()
     {             
-        // KeyVaultSecret secret = secretClient.GetSecret("test-secret");
-        // string secretValue = secret.Value;
+        KeyVaultSecret secret = secretClient.GetSecret("test-secret");
+        string secretValue = secret.Value;
         
         string token = Guid.NewGuid().ToString(); 
-        return $"this is a very super random token: {token} - ";
+        return $"this is a very super random token: {token} - {secretValue}";
     }
 
 
 
 }
+
+// fedc565b565d2cffdf3a1a1a2290de4c1aef5b95
+// fedc565b565d2cffdf3a1a1a2290de4c1aef5b95
