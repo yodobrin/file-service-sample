c8dfdedafdcb9f312340dfd5db193c0d54b38ce3
Yoav Dobrin <yodobrin@microsoft.com>
2022-08-31 15:55:21 +0300

    using mi

--- file-server/Program.cs
+++ file-server/Program.cs
@@ -3,16 +3,16 @@ using Azure.Identity;
 var builder = WebApplication.CreateBuilder(args);
 
 // when running localy and your user is granted right rbac on the keyvault, u can use this:
-builder.Configuration.AddAzureKeyVault(
-        new Uri($"https://{builder.Configuration["keyvault"]}.vault.azure.net/"),
-        new DefaultAzureCredential());
-
 // builder.Configuration.AddAzureKeyVault(
 //         new Uri($"https://{builder.Configuration["keyvault"]}.vault.azure.net/"),
-//         new DefaultAzureCredential(new DefaultAzureCredentialOptions
-//         {
-//             ManagedIdentityClientId = builder.Configuration["AzureADManagedIdentityClientId"]
-//         }));
+//         new DefaultAzureCredential());
+
+builder.Configuration.AddAzureKeyVault(
+        new Uri($"https://{builder.Configuration["keyvault"]}.vault.azure.net/"),
+        new DefaultAzureCredential(new DefaultAzureCredentialOptions
+        {
+            ManagedIdentityClientId = builder.Configuration["AzureADManagedIdentityClientId"]
+        }));
 
 
 // Add services to the container
