ac0421ec7d869f6de6f1da47d99f8103f05156d0
Yoav Dobrin <yodobrin@microsoft.com>
2022-09-04 12:17:59 +0300

    find remote ip

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # file-service-sample
-to be comp later
+
+## User story
+
+(revise as real user story?)
+As a service providers, you require or use files uploaded by your customers. You are aiming for a secured, easy to maintain micro-service, that would be used either as an API or as a UI service.
+
+## Main Desgin Considerations
+
+- Secured, the code must hosted in vnet enabled compute.
+- Youd storage must not be publicly exposed.
+- All uploads are considered unsafe unless verified.
+- Customer uploads must land on DMZ storage, with minimal, automatic clean up.
+
--- file-server/Controllers/SaSTokenController.cs
+++ file-server/Controllers/SaSTokenController.cs
@@ -12,10 +12,9 @@ public class SaSTokenController : ControllerBase
 {
     private readonly ILogger<SaSTokenController> _logger;
     IConfiguration _configuration;
-    // private IHttpContextAccessor _accessor;
     
-    
-
+    private readonly string XENVOY_IP = "X-Envoy-External-Address";
+    private readonly string XFWD4_IP = "x-forwarded-for";
     public SaSTokenController(ILogger<SaSTokenController> logger, IConfiguration configuration)
     {
         _logger = logger;
@@ -34,10 +33,8 @@ public class SaSTokenController : ControllerBase
             Permissions = SharedAccessBlobPermissions.Create | SharedAccessBlobPermissions.Write
         };
         var feature = HttpContext.Features.Get<IHttpConnectionFeature>();
-        var remoteIp = feature?.RemoteIpAddress;
-        PrintHeaders();
-        remoteIp = (remoteIp!=null)?remoteIp:IPAddress.Parse("1.1.1.1");
-
+        var remoteIp = IPAddress.Parse(FindRemoteIp());
+        
         string connectionString = _configuration.GetValue<string>("storagecs");
         string containerName = Guid.NewGuid().ToString();
 
@@ -48,11 +45,23 @@ public class SaSTokenController : ControllerBase
         return $"Container: {containerName} |Period - {_configuration["access_period"]} - ip {remoteIp.MapToIPv6()} |";
     }
 
-    private void PrintHeaders(){
-        foreach (var itm in HttpContext.Request?.Headers)
-        {
-            _logger.LogInformation($"header item {itm.Key} - {itm.Value}|");
+    private string FindRemoteIp(){
+        
+        string remoteIp = "1.1.1.1";
+        IHeaderDictionary? headers = HttpContext.Request?.Headers;
+        if( headers!=null){
+            string xenvoy = headers[XENVOY_IP];
+            string xfwd4 = headers[XFWD4_IP];
+            remoteIp = (xenvoy!=null && xenvoy.Equals(xfwd4))?xenvoy:remoteIp;
+            _logger.LogInformation($"xenvoy={xenvoy}, xfwd4={xfwd4}");
+            foreach (var itm in headers)
+            {
+                
+                _logger.LogInformation($"header item {itm.Key} - {itm.Value}|");
+            }
         }
+        
+        return remoteIp;
 
         
     }
